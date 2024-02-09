9c97298c3084620401a65d445f48e6419e558965
Arsalan Ali <dev.arsalanali@gmail.com>
2024-02-09 23:36:11 +0500

    fix

---
+++ src/components/sections/PopularNfts.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Slider from '../elements/Slider'
+import NftCard from '../elements/NftCard'
+import './style.css'
+
+export const popularSlides = [
+  <NftCard />,
+  <NftCard />,
+  <NftCard />,
+  <NftCard />,
+  <NftCard />,
+  <NftCard />,
+]
+
+export default function PopularNfts() {
+  return (
+    <div className='popular'>
+      <div className='container'>
+        <h2>Popular</h2>
+        <div className='row'>
+          <Slider slides={popularSlides} />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/sections/TrendingNfts.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import TrendingTable from '../elements/TrendingTable'
+import Button from '../elements/Button'
+import ToggleTab from '../elements/ToggleTab'
+import './style.css'
+
+const trendTopOptions = [
+  {
+    option: 'Trending',
+  },
+  {
+    option: 'Top',
+  },
+]
+const dateOptions = [
+  {
+    option: '24 hrs',
+  },
+  {
+    option: '3 days',
+  },
+  {
+    option: '1 week',
+  },
+  {
+    option: 'All time',
+  },
+]
+
+export default function TrendingNfts() {
+  return (
+    <div className='trending'>
+      <div className='container'>
+        <h2>
+          Trending <span className='highlight'>NFTs</span>
+        </h2>
+        <div className='row space-between justify-center'>
+          <ToggleTab options={trendTopOptions} />
+          <ToggleTab options={dateOptions} />
+          <Button label='View All' variant='outlined' />
+        </div>
+
+        <div className='row'>
+          <TrendingTable />
+          <TrendingTable />
+        </div>
+      </div>
+    </div>
+  )
+}
